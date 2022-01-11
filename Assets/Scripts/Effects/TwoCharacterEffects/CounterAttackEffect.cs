ce0b261a777abff2e8dd9bc6da04be64704fde39
yamaz0 <szulc.patryk1c@gmail.com>
2022-01-12 00:24:01 +0100

    Added CounterAttackEffect

---
+++ Assets/Scripts/Effects/TwoCharacterEffects/CounterAttackEffect.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CounterAttackEffect : TwoCharacterEffect
+{
+    [SerializeField]
+    private float exhaustTime;
+    public float CacheTime { get; set; }
+    public Character CacheAtackerCharacter { get; set; }
+    public Character CacheAtackedCharacter { get; set; }
+
+    public override void Execute(Character atacker, Character atacked)
+    {
+        CacheAtackerCharacter = atacker;
+        CacheAtackedCharacter = atacked;
+        atacked.Statistics.Hp.OnLevelChanged += TryReflectDamage;//DO POPRAWY BO NIE MOZE BYC EVENT HP TYLKO ON ATTACK EVENT CZY COS
+    }
+
+    public override void Remove(Character atacker, Character atacked)
+    {
+        atacked.Statistics.Hp.OnLevelChanged -= TryReflectDamage;
+    }
+
+    public void TryReflectDamage(float damage)
+    {
+        if (CacheTime + exhaustTime < Time.fixedTime)
+        {
+            CacheTime = Time.fixedTime;
+        }
+    }
+
+#if UNITY_EDITOR
+    public override void ViewFields()
+    {
+        base.ViewFields();
+    }
+
+    public override void ViewInfo()
+    {
+        base.ViewInfo();
+    }
+#endif
+}
