e4fc09ef4776c0e4705618dada1cb443b5a693b2
yamaz0 <szulc.patryk1c@gmail.com>
2022-01-12 00:54:30 +0100

    Fixed and refactor AddAbilityEffect

--- Assets/Scripts/Character/CharacterAbilities/Abilities.cs
+++ Assets/Scripts/Character/CharacterAbilities/Abilities.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ModificatorType]
+[System.Serializable]
 public class Abilities
 {
     [SerializeField]
--- Assets/Scripts/Effects/OneCharacterEffects/AddAbilityEffect.cs
+++ Assets/Scripts/Effects/OneCharacterEffects/AddAbilityEffect.cs
@@ -2,22 +2,22 @@ using System;
 using UnityEngine;
 
 [System.Serializable]
-public class AddItemEffect : OneCharacterEffect
+public class AddAbilityEffect : OneCharacterEffect
 {
     [SerializeField]
-    [ItemDropdown]
-    private int itemId;
+    [AbilitiesDropdown]
+    private int abilityId;
     [SerializeField]
     private bool isRemove = false;
     public bool IsRemove { get => isRemove; set => isRemove = value; }
-    public int ItemId { get => itemId; set => itemId = value; }
+    public int AbilityId { get => abilityId; set => abilityId = value; }
 
     public override void Execute(Character character)
     {
-        if (IsRemove == true)
-            character.InventoryController.RemoveItem(ItemId);
+        if (IsRemove == false)
+            character.Abilities.KnownAbilities.Add(AbilityId);
         else
-            character.InventoryController.AddItem(ItemId);
+            character.Abilities.KnownAbilities.Remove(AbilityId);
     }
 
     public override void Remove(Character character)
