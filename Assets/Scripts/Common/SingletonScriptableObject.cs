eaa5ce5d49224975bdeec75d672ee3c1b5a33b9d
yamaz0 <szulc.patryk1c@gmail.com>
2022-02-05 19:24:37 +0100

    moved singletonScriptableObject to file

---
+++ Assets/Scripts/Common/SingletonScriptableObject.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SingletonScriptableObject<T> : ScriptableObject where T : SingletonScriptableObject<T>
+{
+    private static T instance;
+
+    [SerializeReference]
+    private List<BaseInfo> objects = new List<BaseInfo>();
+
+    public static T Instance { get => instance; set => instance = value; }
+    public List<BaseInfo> Objects { get => objects; set => objects = value; }
+
+    [RuntimeInitializeOnLoadMethod]
+    protected static void Init()
+    {
+        instance = Resources.LoadAll<T>("")[0];
+    }
+}
--- Assets/Scripts/NPC/ScriptableObject/NPCScriptableObject.cs
+++ Assets/Scripts/NPC/ScriptableObject/NPCScriptableObject.cs
@@ -1,25 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-[System.Serializable]
-public class SingletonScriptableObject<T> : ScriptableObject where T : SingletonScriptableObject<T>
-{
-    private static T instance;
-
-    [SerializeReference]
-    private List<BaseInfo> objects = new List<BaseInfo>();
-
-    public static T Instance { get => instance; set => instance = value; }
-    public List<BaseInfo> Objects { get => objects; set => objects = value; }
-
-    [RuntimeInitializeOnLoadMethod]
-    protected static void Init()
-    {
-        instance = Resources.LoadAll<T>("")[0];
-    }
-}
-
 [CreateAssetMenu(fileName = "NPCs", menuName = "ScriptableObjects/NPCs")]
 [System.Serializable]
 public class NPCScriptableObject : SingletonScriptableObject<NPCScriptableObject>
